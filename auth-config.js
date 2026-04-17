87c3c477fd1daa3059ff1c1039a62700793a7f6c
Rafaelhanson <rafaelhansonfotografo@gmail.com>
2026-04-17 20:06:31 -0300

    Add files via upload

--- auth-config.js
+++ auth-config.js
@@ -3,6 +3,5 @@ window.APP_AUTH_CONFIG = {
   loginPath: "./login.html",
   redirectAfterLogin: "./index.html",
   supabaseUrl: "https://dxdmqarllnljhhddyxsz.supabase.co",
-  supabaseAnonKey: "sb_publishable_sb_publishable_D5e4vSZGg5R-pO1AB819tg_aGEY1ycc"
+  supabaseAnonKey: "sb_publishable_D5e4vSZGg5R-pO1AB819tg_aGEY1ycc"
 };
-
